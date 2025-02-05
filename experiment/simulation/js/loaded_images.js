f1365303f8079245fd45c1fb816290dd8ff8af14
pushpdeep <>
2025-02-05 14:16:09 +0000

    added payload

---
+++ experiment/simulation/js/loaded_images.js
@@ -0,0 +1,17 @@
+//all images
+var counter = new Image();
+counter.src = `./images/webp/ElectronicCounter02.webp`;
+var tube = new Image();
+tube.src = `./images/webp/CathodeGMtube.webp`;
+var resistance = new Image();
+resistance.src = `./images/webp/Resistance.webp`;
+var capacitance = new Image();
+capacitance.src = `./images/webp/capacitance.webp`;
+var hv = new Image();
+hv.src = `./images/webp/Highvoltage.webp`;
+var anode = new Image();
+anode.src = `./images/webp/Anode.webp`;
+// assembly image
+var assembly = new Image();
+assembly.src = `./images/webp/Setup2.webp`;
+//# sourceMappingURL=loaded_images.js.map
