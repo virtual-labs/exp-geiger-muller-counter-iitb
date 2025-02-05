e750fa1f715b4eb6c9676940e0082fb4041492d0
jaimathur <mathurjai88@gmail.com>
2025-02-05 19:32:15 +0530

    deleted ts and js directory

--- experiment/simulation/js/data.js
+++
@@ -1,4 +0,0 @@
-var selected_start_voltage = 0;
-var gm_table = [];
-var main_data_table = [];
-//# sourceMappingURL=data.js.map
--- experiment/simulation/js/dummy.js
+++
@@ -1,6 +0,0 @@
-var cc = document.getElementById("pannelcreate");
-var pp = new Pannel(cc);
-pp.addoffcanvas(1);
-pp.addoffcanvas(2);
-pp.addoffcanvas(3);
-//# sourceMappingURL=dummy.js.map
